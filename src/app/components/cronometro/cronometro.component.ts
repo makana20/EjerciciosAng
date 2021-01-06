cf3d9ae5c0c3b84643d4f8e757d849b57732fec3
Omar Pérez <operez@teigps.cl>
2021-01-05 23:11:00 -0300

    clase 26

--- src/app/components/cronometro/cronometro.component.ts
+++ src/app/components/cronometro/cronometro.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cronometro',
+  selector: 'cronometro',
   templateUrl: './cronometro.component.html',
   styleUrls: ['./cronometro.component.css']
 })
 export class CronometroComponent implements OnInit {
 
-  constructor() { }
+    contador: number;
+
+  constructor() { 
+    this.contador = 12;
+  }
 
   ngOnInit(): void {
+  }
+
+  onClinck(){
+    let interval = setInterval(()=> {
+      this.contador--
+      if(this.contador === 0 ){
+        clearInterval(interval);
+
+      }
+    }, 1000);
+
   }
 
 }
